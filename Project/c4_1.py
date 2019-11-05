c17193db956d19fe9c6436a61f710a62ee989abd
alcome1614 <48035687+alcome1614@users.noreply.github.com>
2019-11-05 10:08:00 +0100

    Add files via upload

---
+++ Project/c4_1.py
@@ -0,0 +1,173 @@
+import numpy as np
+
+
+#strategy 1
+def create_matrix(G,C,z,n,m,N):
+# def create_strat1_matrix(G,C,z,n,m):
+    lbd = z[-2*m:-m]
+    s = z[-m:]
+    matrix = np.zeros((n+m,n+m))
+    matrix[:n,:n] = G
+    matrix[:n,n:] = -C
+    matrix[n:,:n] = -C.T
+    matrix[-m:,-m:] = np.diag(-s/lbd)
+    return matrix
+
+def create_small_rhv(z,rhv,n,m,N):
+# def create_strat1_rhv(z,G,C,g,d,n,m,N):
+    lbd = z[-2*m:-m]
+    r1 = -rhv[:n]
+    r2 = -rhv[-2*m:-m]
+    r3 = -rhv[-m:]
+    small_rhv = np.zeros(n+m)
+    small_rhv[:n] = -r1
+    small_rhv[-m:] = -r2 + r3/lbd
+    return small_rhv
+
+def create_rhv(z,G,C,g,d,n,m,N):
+    x = z[:n]
+    lbd = z[-2*m:-m]
+    s = z[-m:]
+    rhv = np.zeros(N)
+    rhv[:n] = np.matmul(G,x) + g - np.matmul(C,lbd)
+    rhv[-2*m:-m] = s + d - np.matmul(C.T,x)
+    rhv[-m:] = s*lbd
+    return -rhv
+
+def create_dz(z,dz_small,rhv,n,m,N):
+    lbd = z[-2*m:-m]
+    s = z[-m:]
+    r3 = -rhv[-m:]
+    dz = np.zeros(N)
+    dz[:n+m] = dz_small
+    dz[-m:] = (-r3 - s*dz[-2*m:-m]) / lbd
+    return dz
+
+def lu_np(A):
+#Without pivoting
+    n = len(A)
+    A =A.astype('float')
+    for i in range(n-1):
+        aii = A[i,i]
+        for j in range(i+1,n):
+            A[j,i] = A[j,i]/aii
+        for j in range(i+1,n):
+            for k in range(i+1,n):
+                A[j,k] = A[j,k]-A[j,i]*A[i,k]
+    L = np.tril(A,-1)+np.eye(n)
+    U = np.triu(A)
+    return L,U
+
+def ldl(A):
+    n = len(A)
+    L,U = lu_np(A)
+    diag_U = np.array([U[i,i] for i in range(n)])
+    D = np.diag(diag_U)
+    LT = np.array([U[i]/diag_U[i] for i in range(n)])
+    return L,D,LT
+
+def frwd_subs(L,b):
+    n = len(L)
+    x = np.zeros(n)
+    x[0] = b[0]/L[0,0]
+    for i in range(1,n):
+        x[i]= b[i]
+        for j in range(i):
+            x[i] = x[i] - L[i,j]*x[j]
+        x[i] = x[i]/L[i,i]
+    return x
+
+def bckwd_subs(U,b):
+    n = len(U)
+    x = np.zeros(n)
+    x[-1] =  b[-1]/U[-1,-1]
+    for i in range(2,n+1):
+        x[-i] = b[-i]
+        for j in range(1,i):
+            x[-i] = x[-i] - U[-i,-j]*x[-j]
+        x[-i] = x[-i]/U[-i,-i]
+    return x
+
+def strat1_ldl(A,b):
+    n = len(A)
+    L,D,LT = ldl(A)
+    DLTX = frwd_subs(L,b)
+    LTX = np.array([DLTX[i]/D[i,i] for i in range(n)])
+    dz  = bckwd_subs(LT,LTX)
+    return dz
+
+def Newton_step(lamb0,dlamb,s0,ds):
+    alp=1;
+    idx_lamb0=np.array(np.where(dlamb<0))
+    if idx_lamb0.size>0:
+        alp = min(alp,np.min(-lamb0[idx_lamb0]/dlamb[idx_lamb0]))
+    idx_s0=np.array(np.where(ds<0))
+    if idx_s0.size>0:
+        alp = min(alp,np.min(-s0[idx_s0]/ds[idx_s0]))
+    return alp
+
+
+def  algorithm(z,hessian,rhv,G,C,g,d,n,m,N):
+    epsilon = 1e-16
+    lbd = z[-2*m:-m]
+    s = z[-m:]
+    #predictor substep
+    small_rhv = create_small_rhv(z,rhv,n,m,N)
+    d_z_small = strat1_ldl(hessian,small_rhv)
+    d_z = create_dz(z,d_z_small,rhv,n,m,N)
+    #step-size correction substep
+    d_lbd = d_z[-2*m:-m]
+    d_s = d_z[-m:]
+    alpha = Newton_step(lbd,d_lbd,s,d_s)
+    #3things
+    mu = np.matmul(s,lbd)/m
+    mu_tilda = np.matmul(s+alpha*d_s,lbd+alpha*d_lbd)/m
+    sigma = (mu_tilda/mu)**3
+    #corrector substep
+    rhv[-m:] = rhv[-m:] - d_s*d_lbd + sigma*mu
+    small_rhv = create_small_rhv(z,rhv,n,m,N)
+    d_z_small = strat1_ldl(hessian,small_rhv)
+    d_z = create_dz(z,d_z_small,rhv,n,m,N)
+    #step-size correction substep
+    d_lbd = d_z[-2*m:-m]
+    d_s = d_z[-m:]
+    alpha = Newton_step(lbd,d_lbd,s,d_s)
+    #update substep
+    z = z + 0.95*alpha*d_z
+    hessian = create_matrix(G,C,z,n,m,N)
+    rhv = create_rhv(z,G,C,g,d,n,m,N)
+    norm_rL = np.linalg.norm(rhv[:n])
+    #norm_rC = np.linalg.norm(rhv[n:n+p])
+    if norm_rL<epsilon:
+        print('norm_rL')
+        return False, z, hessian, rhv
+    #if norm_rC<epsilon and rhv[n:n+p].size>0 :
+    #    print('rC:{} shape:{} norm_rC:{}'.format(rhv[n:n+p],rhv[n:n+p].shape,norm_rC))
+     #   return False, z, hessian, rhv
+    if  abs(mu)<epsilon:
+        print('mu')
+        return False, z, hessian, rhv
+    return True, z, hessian, rhv
+
+def c4_1(n):
+    m = 2*n
+    N = n + 2*m
+    G = np.eye(n)
+    C = np.zeros((n,m))
+    C[:,:n] = np.eye(n)
+    C[:,n:] = -np.eye(n)
+    d = np.array([-10]*m)
+    g = np.random.rand(n)
+    z = np.zeros(N)
+    z[-2*m:] = 1
+    small_KKT = create_matrix(G,C,z,n,m,N)
+    niter = 100
+    loop = True
+    rhv = create_rhv(z,G,C,g,d,n,m,N)
+    iloop=0
+    while loop and iloop<niter:
+        loop, z, small_KKT, rhv = algorithm(z,small_KKT,rhv,G,C,g,d,n,m,N)
+        iloop = iloop+1
+    print('n:{} iters:{} z+g:{}'.format(n,iloop,np.linalg.norm(z[:n]+g)))
+    return
+c4_1(3)
